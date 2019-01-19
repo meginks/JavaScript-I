89861b69a691df80359beb0816d4f90b8bc01bd6
unknown <38929577+meginks@users.noreply.github.com>
2019-01-18 18:48:48 -0600

    finished arrays.js challenges 3-5

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -90,17 +90,27 @@ console.log(findLastCarMakeModel());
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-console.log();
+carModels = inventory.map(function(car) {return car.car_model});
+carModels = carModels.sort();
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [];
-console.log();
+let carYears = []; 
+carYears = inventory.map(function(car) {return car.car_year});
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars =[];
-console.log(); 
+
+let checkCar = function (car_year) {
+    return car_year <= 2000;
+}
+
+oldCars = carYears.filter(checkCar);
+
+console.log(oldCars); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
