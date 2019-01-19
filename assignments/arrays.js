958712cc4d02bffceaa04826655a4dc0a07ac505
unknown <38929577+meginks@users.noreply.github.com>
2019-01-18 18:26:35 -0600

    finished arrays.js challenge 2

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -80,10 +80,12 @@ console.log(findCar33());
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = 0; 
 
+let findLastCarMakeModel = function() {
+   let lastCar = inventory.pop(); 
+   return "The last car is a " + lastCar.car_make + " " + lastCar.car_model;
+}
 
-
-
-console.log();
+console.log(findLastCarMakeModel());
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
